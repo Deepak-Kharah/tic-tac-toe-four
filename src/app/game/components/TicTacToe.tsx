2bfc348b1883047cf9e9265e087496e50170be21
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-04-28 15:15:19 +0530

    feat: add new game support

--- src/app/game/components/TicTacToe.tsx
+++ src/app/game/components/TicTacToe.tsx
@@ -4,11 +4,23 @@ import { effect } from "@preact/signals";
 import classNames from "classnames";
 import { useState } from "react";
 import { didWin } from "../gameLogic";
-import { board } from "../signal";
+import { board, getDefaultBoard } from "../signal";
 import { Row } from "./Row";
 import styles from "./TicTacToe.module.css";
 import { isXTurn as isXTurnSignal } from "../signal";
 import { Circle, Cross } from "./Pieces";
+import { resetGameComponents } from "./Cell";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function TicTacToe() {
   const [gameOver, setGameOver] = useState(false);
@@ -21,9 +33,16 @@ function TicTacToe() {
     } else if (isXTurn !== isXTurnSignal.value) setIsXTurn(isXTurnSignal.value);
   });
 
+  function resetGame() {
+    setGameOver(false);
+    board.value = getDefaultBoard();
+    isXTurnSignal.value = true;
+    resetGameComponents();
+  }
+
   return (
     <>
-      <section className="flex gap-2 py-5 items-center">
+      <section className="flex gap-2 items-center">
         <div
           className={classNames("size-20 flex items-center justify-center", {
             [styles.active]: isXTurn,
@@ -42,7 +61,7 @@ function TicTacToe() {
           <Circle />
         </div>
       </section>
-      <div
+      <section
         className={classNames("flex flex-col gap-2", {
           [styles["game-over"]]: !!gameOver,
         })}
@@ -50,7 +69,49 @@ function TicTacToe() {
         {Array.from({ length: 3 }).map((_, idx) => {
           return <Row key={idx} row={idx} />;
         })}
-      </div>
+      </section>
+      <section>
+        {!!gameOver ? (
+          <button
+            className={classNames(
+              "px-4 py-2 text-sm font-light rounded-lg   hover:text-white hover:bg-blue-900 transition-all bg-blue-900/70 text-slate-300 shadow-lg"
+            )}
+            onClick={resetGame}
+          >
+            New game
+          </button>
+        ) : (
+          <Dialog>
+            <DialogTrigger
+              className={classNames(
+                "px-4 py-2 text-sm font-light rounded-lg   hover:text-white hover:bg-blue-900  transition-all bg-gray-900/60 text-gray-500/80"
+              )}
+            >
+              New game
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Start over?</DialogTitle>
+              </DialogHeader>
+              <DialogDescription>
+                The game didn&apos;t finish yet. Are you sure you want to start
+                a new game? All progress will be lost
+              </DialogDescription>
+              <DialogFooter>
+                <DialogClose className="px-4 py-2 text-sm font-light rounded-lg hover:text-white hover:bg-gray-900 transition-all">
+                  Cancel
+                </DialogClose>
+                <DialogClose
+                  onClick={resetGame}
+                  className="px-4 py-2 text-sm font-light rounded-lg bg-blue-900/70 text-slate-300 shadow-lg hover:bg-blue-900 transition-all"
+                >
+                  Start new game
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        )}
+      </section>
     </>
   );
 }
